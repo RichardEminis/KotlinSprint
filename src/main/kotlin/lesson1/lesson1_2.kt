09ff17ea0c15eec19a3e0dd73102b58d297d432b
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-19 00:30:35 +0300

    KS-1-2 upgrade my first task

---
+++ src/main/kotlin/lesson1/lesson1_2.kt
@@ -0,0 +1,14 @@
+package lesson1
+
+fun main() {
+    var numberOfOrders: Int = 75
+    val notificationText: String = "Вы сегодня хорошо поработали!"
+
+    println(numberOfOrders )
+    println(notificationText )
+
+    //var nuberOfEmployees: Int = 2000
+    var nuberOfEmployees: Int = 1999
+
+    println(nuberOfEmployees)
+}
