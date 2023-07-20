ffdcd201506c22276ca6c1f71b2b0f6e50886b5c
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-20 22:43:15 +0300

    KS-1-5 Convert seconds to format

--- src/main/kotlin/lesson1/lesson1_5.kt
+++ src/main/kotlin/lesson1/lesson1_5.kt
@@ -2,16 +2,12 @@ package lesson1
 
 fun main() {
     var secondsInSpace: Int = 6480
-    val hoursInSpace: Int = (secondsInSpace / 60) / 60
-    val minutesInSpace: Int = (secondsInSpace/60) - (60*hoursInSpace)
-    var seconds: Int = minutesInSpace*60 - (secondsInSpace - 3600*hoursInSpace)
-    seconds = Math.abs(seconds)
-
-    if((seconds < 10)&&(hoursInSpace < 10)) {
-        println("ГАГАРИН НАХОДИЛСЯ В КОСМОСЕ 0$hoursInSpace:$minutesInSpace:0$seconds")
-    } else if ((seconds < 10)&&(hoursInSpace >= 10)){
-        println("ГАГАРИН НАХОДИЛСЯ В КОСМОСЕ $hoursInSpace:$minutesInSpace:0$seconds")
-    } else {
-        println("ГАГАРИН НАХОДИЛСЯ В КОСМОСЕ $hoursInSpace:$minutesInSpace:$seconds")
-    }
+    var time = String.format(
+        "ГАГАРИН НАХОДИЛСЯ В КОСМОСЕ %02d:%02d:%02d",
+        secondsInSpace / 3600,
+        secondsInSpace / 60 % 60,
+        secondsInSpace % 60
+    );
+//крутая штука, я заценил :D
+    print(time)
 }
