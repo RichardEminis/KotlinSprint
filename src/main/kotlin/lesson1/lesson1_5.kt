291e099e2cd005d323435f8cacfaa75e5192901e
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-21 23:29:32 +0300

    KS-1-5 Gagarin in Space (Edited)

--- src/main/kotlin/lesson1/lesson1_5.kt
+++ src/main/kotlin/lesson1/lesson1_5.kt
@@ -1,13 +1,8 @@
 package lesson1
 
 fun main() {
-    var secondsInSpace: Int = 6480
-    var time = String.format(
-        "ГАГАРИН НАХОДИЛСЯ В КОСМОСЕ %02d:%02d:%02d",
-        secondsInSpace / 3600,
-        secondsInSpace / 60 % 60,
-        secondsInSpace % 60
-    );
-//крутая штука, я заценил :D
+    val secondsInSpace: Int = 6480
+    val time = String.format("ГАГАРИН НАХОДИЛСЯ В КОСМОСЕ %02d:%02d:%02d",secondsInSpace / 3600, secondsInSpace / 60 % 60, secondsInSpace % 60)
+
     print(time)
 }
