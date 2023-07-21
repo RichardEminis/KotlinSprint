b1a191316d075e68ef55be8b344d3d08bfd5b131
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-21 23:16:38 +0300

    KS-2-4 Print 20% buff for ore

--- src/main/kotlin/lesson2/lesson2_4.kt
+++ src/main/kotlin/lesson2/lesson2_4.kt
@@ -1,5 +1,11 @@
 package lesson2
 
 fun main() {
+    val ironOre: Int = 11
+    val crystalОre: Int = 7
+    val buffOfIron: Double = (ironOre / 100.0) * 20
+    val buffOfCristal: Double = (crystalОre / 100.0) * 20
 
+    println("Добыто железной руды = $ironOre; Добыто руды под баффом = ${ironOre + buffOfIron.toInt()}; Бонус = ${buffOfIron.toInt()}")
+    println("Добыто кристаллической руды = $crystalОre; Добыто руды под баффом = ${crystalОre + buffOfCristal.toInt()}; Бонус = ${buffOfCristal.toInt()}")
 }
