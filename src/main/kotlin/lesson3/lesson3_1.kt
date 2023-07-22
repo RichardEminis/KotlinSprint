0dd73a06922de8a407efc869480c25cf50d0609d
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-22 23:10:30 +0300

    KS-3-1 First data

---
+++ src/main/kotlin/lesson3/lesson3_1.kt
@@ -0,0 +1,19 @@
+package lesson3
+
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+
+fun main() {
+    val currentTime = Date()
+    val nightTime = "02:01:04"
+    val morningTime = "06:00"
+    val dayTime = "12:00"
+    val eveningTime = "18:00"
+    val eveningTime2 = "24:00"
+
+    }
+
