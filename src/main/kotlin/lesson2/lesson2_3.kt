7ce533bda8157c09df8e3c4140567d26bbb64a5e
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-22 20:05:43 +0300

    KS-2-3 Arriving train time (Edited)

--- src/main/kotlin/lesson2/lesson2_3.kt
+++ src/main/kotlin/lesson2/lesson2_3.kt
@@ -1,19 +1,14 @@
 package lesson2
 
-package lesson2
-
+const val SIXTY_UNITS_OF_TIME: Int = 60
 fun main() {
     val hour: Int = 9
     val minute: Int = 39
     val travelTime: Int = 457
-    val timeToStart: Int = (hour * 60) + minute
+    val timeToStart: Int = (hour * SIXTY_UNITS_OF_TIME) + minute
     val sumOfTime: Int = timeToStart + travelTime
 
-    var time = String.format(
-        "ВРЕМЯ ПРИБЫТИЯ ПОЕЗДА %02d:%02d",
-        sumOfTime / 60,
-        sumOfTime % 60,
-    );
+    val time = String.format("ВРЕМЯ ПРИБЫТИЯ ПОЕЗДА %02d:%02d", sumOfTime / SIXTY_UNITS_OF_TIME, sumOfTime % SIXTY_UNITS_OF_TIME)
 
     println(time)
 }
