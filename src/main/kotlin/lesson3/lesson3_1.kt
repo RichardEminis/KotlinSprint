c8f6194d96b9333f9acc3b971f235f4a494ebccf
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-23 14:18:44 +0300

    KS-3-1 Greetings

--- src/main/kotlin/lesson3/lesson3_1.kt
+++ src/main/kotlin/lesson3/lesson3_1.kt
@@ -1,19 +1,31 @@
 package lesson3
 
-import java.text.DateFormat
-import java.text.SimpleDateFormat
 import java.time.LocalTime
-import java.time.format.DateTimeFormatter
-import java.util.*
-
 
 fun main() {
-    val currentTime = Date()
-    val nightTime = "02:01:04"
-    val morningTime = "06:00"
-    val dayTime = "12:00"
-    val eveningTime = "18:00"
-    val eveningTime2 = "24:00"
+    val currentTime = LocalTime.now()
+    val nightTime = LocalTime.parse("00:00")
+    val morningTime = LocalTime.parse("06:00")
+    val dayTime = LocalTime.parse("12:00")
+    val eveningTime = LocalTime.parse("18:00")
 
+    val nightText: String = ", сладких снов :3"
+    val morningText: String = "Доброе утро,"
+    val dayText: String = "Добрый день,"
+    val eveningText: String = "Добрый вечер,"
+
+    println("Введите свое имя")
+    val name = readln()
+    println()
+
+    if ((currentTime.isAfter(nightTime)) && (currentTime.isBefore(morningTime))) {
+        println("$name $nightText")
+    } else if ((currentTime.isAfter(morningTime)) && (currentTime.isBefore(dayTime))) {
+        println("$morningText $name")
+    } else if ((currentTime.isAfter(dayTime)) && (currentTime.isBefore(eveningTime))) {
+        println("$dayText $name")
+    } else {
+        println("$eveningText $name")
     }
+}
 
