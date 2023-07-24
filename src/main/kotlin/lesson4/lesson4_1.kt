881443eba5d538de7f6d26dea05138156dc9894f
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-24 23:17:46 +0300

    KS-5-3 KS-4-1 Table reservation (Edited)

--- src/main/kotlin/lesson4/lesson4_1.kt
+++ src/main/kotlin/lesson4/lesson4_1.kt
@@ -1,14 +1,14 @@
 package lesson4
 
+const val NUMBER_OF_TABLES: Int = 13
 fun main() {
-    val numberOfTables: Int = 13
     val reservedTablesToday: Int = 13
     val reservedTablesTomorrow: Int = 9
-    var freeTables: Boolean = reservedTablesToday < numberOfTables
+    var freeTables: Boolean = reservedTablesToday < NUMBER_OF_TABLES
 
     println("Доступность столиков на сегодня: $freeTables")
 
-    freeTables = reservedTablesTomorrow < numberOfTables
+    freeTables = reservedTablesTomorrow < NUMBER_OF_TABLES
 
     println("Доступность столиков на завтра: $freeTables")
 }
