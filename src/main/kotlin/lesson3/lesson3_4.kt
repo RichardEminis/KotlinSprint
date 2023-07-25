9137453d6051c70a0ab18a3411a036001cd52f0e
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-25 23:48:19 +0300

    KS-3-4 Chess (Edited)

--- src/main/kotlin/lesson3/lesson3_4.kt
+++ src/main/kotlin/lesson3/lesson3_4.kt
@@ -3,14 +3,16 @@ package lesson3
 fun main() {
     var startPoint: String = "E2"
     var endPoint: String = "E4"
-    var moveResult: String = "$startPoint $endPoint"
+    var moveNumber: Int = 1
+    var moveResult: String = "$startPoint-$endPoint;$moveNumber"
 
     println(moveResult)
 
     startPoint = "D1"
     endPoint = "D2"
+    moveNumber++
 
-    moveResult = "$startPoint $endPoint"
+    moveResult = "$startPoint-$endPoint;$moveNumber"
 
     println(moveResult)
 }
