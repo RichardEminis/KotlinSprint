38127ecbedd6b7aece7d9ede5fa514a4ca4bace7
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-25 23:35:00 +0300

    KS-5-1 Authorization (Edited)

--- src/main/kotlin/lesson5/lesson5_1.kt
+++ src/main/kotlin/lesson5/lesson5_1.kt
@@ -1,13 +1,14 @@
 package lesson5
 
+import java.lang.Integer.sum
+
 fun main() {
     val firstNumber: Int = 2
     val secondNumber: Int = 5
     println("Решите пример для доступа: $firstNumber + $secondNumber = ? ")
     val answerOfUser: Int = readln().toInt()
-    val expectedAnswer: Int = firstNumber + secondNumber
 
-    if (answerOfUser == expectedAnswer) {
+    if (answerOfUser == (sum(firstNumber, secondNumber))) {
         println("Добро полжаловать!")
     } else {
         println("Доступ запрещен.")
