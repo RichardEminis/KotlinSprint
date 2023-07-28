b003dc9626c9b37422655076c741b662796827c9
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-28 23:14:25 +0300

    KS-4-5 Ship status (Edited)

--- src/main/kotlin/lesson4/lesson4_5.kt
+++ src/main/kotlin/lesson4/lesson4_5.kt
@@ -1,6 +1,6 @@
 package lesson4
 
-const val NO_DAMAGE: Boolean = true
+const val HAS_NOT_DAMAGE: Boolean = true
 const val MIN_NUMBER_OF_CREW: Int = 55
 const val MAX_NUMBER_OF_CREW: Int = 70
 const val MIN_NUMBER_OF_PROVISIONS: Int = 50
@@ -8,24 +8,24 @@ const val WEATHER_IS_FAVORABLE: Boolean = true
 
 fun main() {
     val welcomeText: String = "Здравствуйте! Введите характеристики состояния вашего судна."
-    val statusOfDamageText: String = "Ваше судно не имеет повреждений?"
-    println("$welcomeText\n$statusOfDamageText")
-    val statusOfDamage: Boolean = readln().toBoolean()
+    val hasNotDamageText: String = "Ваше судно не имеет повреждений?"
+    println("$welcomeText\n$hasNotDamageText")
+    val hasNotDamage: Boolean = readln().toBoolean()
     val numberOfCrewText: String = "Укажите количество членов экипажа"
     println("$numberOfCrewText")
     val numberOfCrew: Int = readln().toInt()
     val numberOfProvisionText: String = "Укажите количество провизи на борту"
     println("$numberOfProvisionText")
     val numberOfProvision: Int = readln().toInt()
-    val statusOfWeatherText: String = "Введите благоприятны ли метеоусловия?"
-    println("$statusOfWeatherText")
-    val statusOfWeather: Boolean = readln().toBoolean()
+    val weatherIsGoodText: String = "Введите благоприятны ли метеоусловия?"
+    println("$weatherIsGoodText")
+    val weatherIsGood: Boolean = readln().toBoolean()
 
-    val maxComplianceCheck: Boolean = (MIN_NUMBER_OF_CREW <= numberOfCrew) && (MAX_NUMBER_OF_CREW >= numberOfCrew)
-            && (MIN_NUMBER_OF_PROVISIONS < numberOfProvision) && (WEATHER_IS_FAVORABLE == statusOfWeather)
-            && (NO_DAMAGE == statusOfDamage)
-    val minComplianceCheck: Boolean = (MAX_NUMBER_OF_CREW == numberOfCrew) && (WEATHER_IS_FAVORABLE == statusOfWeather)
-            && (MIN_NUMBER_OF_PROVISIONS < numberOfProvision)
+    val maxComplianceCheck: Boolean = (MIN_NUMBER_OF_CREW <= numberOfCrew) && (MAX_NUMBER_OF_CREW >= numberOfCrew) &&
+            (MIN_NUMBER_OF_PROVISIONS < numberOfProvision) && (WEATHER_IS_FAVORABLE == weatherIsGood) &&
+            (HAS_NOT_DAMAGE == hasNotDamage)
+    val minComplianceCheck: Boolean = (MAX_NUMBER_OF_CREW == numberOfCrew) &&
+            (WEATHER_IS_FAVORABLE == weatherIsGood) && (MIN_NUMBER_OF_PROVISIONS < numberOfProvision)
     val finComplianceCheck: Boolean = maxComplianceCheck || minComplianceCheck
 
     println("Готовность корабля к долгосрочному плаванию: $finComplianceCheck")
