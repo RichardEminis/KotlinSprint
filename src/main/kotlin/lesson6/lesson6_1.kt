024bc7f3afe81995c35c872a7384bb5a967b2db1
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-29 00:10:31 +0300

    KS-6-1 LogIn and password

---
+++ src/main/kotlin/lesson6/lesson6_1.kt
@@ -0,0 +1,31 @@
+package lesson6
+
+fun main() {
+    val userLogInText = "Здравствуйте! Для входа необходимо зарегистрироваться.\nВведите имя пользователя:"
+    println(userLogInText)
+    val userLogIn = readln()
+    val userPasswordText = "Введите парль:"
+    println(userPasswordText)
+    val userPassword = readln()
+    val congratulationText = "Поздравляем, учетная запись создана!"
+    println(congratulationText)
+    println()
+
+    do {
+        val startTest = "Для входа введите имя пользователя:"
+        println(startTest)
+        val logIn: String = readln()
+        val passwordText = "Введите пароль:"
+        println(passwordText)
+        val password: String = readln()
+
+        if ((logIn != userLogIn) && (password != userPassword)) {
+            val mistakeText = "Неверный логин или пароль, попробуйте авторизоваться заново."
+            println(mistakeText)
+            println()
+        } else {
+            val rightLogInText = "Авторизация прошла успешно!"
+            println(rightLogInText)
+        }
+    } while ((userLogIn != logIn) && (userPassword != password))
+}
