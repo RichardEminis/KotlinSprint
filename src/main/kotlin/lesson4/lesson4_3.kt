85eea4842a7ae47be1f015c470524748cfe22a39
Aleksi <eminis.ru@gmail.com>
2023-07-29 13:58:54 +0300

    KS-4-3 Conditions for beans (Edited)

--- src/main/kotlin/lesson4/lesson4_3.kt
+++ src/main/kotlin/lesson4/lesson4_3.kt
@@ -1,16 +1,16 @@
 package lesson4
 
-const val WEATHER_IS_SUNNY: Boolean = true
-const val TENT_IS_OPEN: Boolean = true
+const val IS_SUNNY: Boolean = true
+const val IS_OPEN: Boolean = true
 const val AIR_HUMIDITY: Int = 20
-val SEASON_IS_NOT_FAVORABLE: String = "Зима"
+val IS_NOT_FAVORABLE: String = "Зима"
 fun main() {
     val currentSeason: String = "Зима"
     val airHumidity: Int = 20
-    val weatherIsSunny: Boolean = true
-    val tentIsOpen: Boolean = true
-    val conditions: Boolean = (WEATHER_IS_SUNNY && weatherIsSunny) && (TENT_IS_OPEN && tentIsOpen)
-            && (SEASON_IS_NOT_FAVORABLE != currentSeason) && (airHumidity == AIR_HUMIDITY)
+    val isSunny: Boolean = true
+    val isOpen: Boolean = true
+    val conditions: Boolean = (IS_SUNNY && isSunny) && (IS_OPEN && isOpen)
+            && (IS_NOT_FAVORABLE != currentSeason) && (airHumidity == AIR_HUMIDITY)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $conditions")
 }
