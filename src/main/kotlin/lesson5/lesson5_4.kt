c8839f173600c705fa040b5e7d58cd14c7bcd93b
Alexandr Belyaev <eminis.ru@gmail.com>
2023-07-30 13:21:52 +0300

    KS-5-4 LogIn for PanGalactic (Edited)

--- src/main/kotlin/lesson5/lesson5_4.kt
+++ src/main/kotlin/lesson5/lesson5_4.kt
@@ -1,6 +1,22 @@
 package lesson5
 
+const val USER_LOGIN: String = "Zaphod"
+const val USER_PASSWORD: String = "PanGalactic"
+
 fun  main() {
-    val login: String = "Zaphod"
-    val password: String = "PanGalactic"
+    println("Введите имя пользователя:")
+    val logIn: String = readln()
+
+    if (logIn == USER_LOGIN) {
+        println("Введите пароль:")
+        val password: String = readln()
+
+        if (password == USER_PASSWORD) {
+            print("Поздравляю! Вы авторизованы!")
+        } else {
+            print("Неверный пароль.")
+        }
+    } else {
+        print("Вероятно, вашей учетной записи не существует, просьба зарегистрироваться.")
+    }
 }
