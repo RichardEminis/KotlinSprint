0854210271c8bad7d7b282e37a35c005d6a2e5b1
Aleksi <eminis.ru@gmail.com>
2023-08-01 11:44:13 +0300

    KS-6-4 Randomizer The GAME (Edited)

--- src/main/kotlin/lesson6/lesson6_4.kt
+++ src/main/kotlin/lesson6/lesson6_4.kt
@@ -1,18 +1,18 @@
 package lesson6
 
-fun main () {
-    var number_of_attempts: Int = 5
+fun main() {
+    var numberOfAttempts: Int = 5
     val number: Int = (1..10).random()
 
-    while (number_of_attempts != 0) {
+    while (numberOfAttempts > 0) {
         println("Введите число от 1 до 9")
         val userNumber: Int = readln().toInt()
 
         if (userNumber == number) {
             println("Это была великолепная игра!")
-            number_of_attempts = 0
+            return
         } else {
-            println("Неверно\nОставшееся количество попыток: ${--number_of_attempts}")
+            println("Неверно\nОставшееся количество попыток: ${--numberOfAttempts}")
         }
     }
     print("Загаданное число: $number")
