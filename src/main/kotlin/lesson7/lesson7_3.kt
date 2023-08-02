de75dc21d5f50227e7e0f7fa510547f71c88b7f3
Alexandr Belyaev <eminis.ru@gmail.com>
2023-08-02 22:37:56 +0300

    KS-7-3 Progression (Edited)

--- src/main/kotlin/lesson7/lesson7_3.kt
+++ src/main/kotlin/lesson7/lesson7_3.kt
@@ -3,9 +3,8 @@ package lesson7
 fun main() {
     println("Введите число")
     val userNumber: Int = readln().toInt()
-    val progression: IntProgression = 0 until userNumber step 2
 
-    for (i in progression) {
+    for (i in 0 until userNumber step 2) {
         println(i)
     }
 }
