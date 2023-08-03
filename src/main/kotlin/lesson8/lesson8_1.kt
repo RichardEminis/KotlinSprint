131b453658c2893d2ff5424ff8510e92736e14f4
Alexandr Belyaev <eminis.ru@gmail.com>
2023-08-03 08:52:55 +0300

    KS-8-1 Sum of views

--- src/main/kotlin/lesson8/lesson8_1.kt
+++ src/main/kotlin/lesson8/lesson8_1.kt
@@ -1,11 +1,11 @@
 package lesson8
 
 fun main () {
-    var SumOfViews: Int = 0
+    var sumOfViews: Int = 0
     var weeklyViews: IntArray = intArrayOf(4, 6, 2, 1, 7, 8, 14)
 
     for (i in weeklyViews) {
-        SumOfViews = SumOfViews + i
+        sumOfViews = sumOfViews + i
     }
-    println("$SumOfViews")
+    println("$sumOfViews")
 }
