ba5bb83636fa4c2e1b71960dec4c88ee68d96776
Alexandr Belyaev <eminis.ru@gmail.com>
2023-08-03 20:07:46 +0300

    KS-8-3 Upgrade ingredients finder

---
+++ src/main/kotlin/lesson8/lesson8_3.kt
@@ -0,0 +1,14 @@
+package lesson8
+
+fun main () {
+    val arrayOfIngredients = arrayOf("Морковь", "Свекла", "Помидоры", "Мазик", "Соль", "Любовь")
+
+    println("Введите искомый ингредиент:")
+    val ingredient = readln()
+
+    if (arrayOfIngredients.contains(ingredient) == true){
+        println("Ингредиент [$ingredient] в рецепте есть")
+    } else {
+        println("Такого ингредиента в рецепте нет")
+    }
+}
