444606efe93d7e8489bfe4f466cff3cf13bff9bc
Aleksi <eminis.ru@gmail.com>
2023-08-05 11:40:45 +0300

    KS-7-2 SMS Generator (Edited)

--- src/main/kotlin/lesson7/lesson7_2.kt
+++ src/main/kotlin/lesson7/lesson7_2.kt
@@ -1,24 +1,18 @@
 package lesson7
 
-fun main (){
-    var smsNumbers: String = (1..9).random().toString()
+fun main() {
+    var smsNumbers = (1000..9999).random()
 
-    do{
-        for (i in 2 downTo 0 ){
-            var storageSmsNumbers:String = smsNumbers
-            smsNumbers = (1..9).random().toString()
-            smsNumbers += storageSmsNumbers
-        }
+    do {
         println("Ваш код авторизации: $smsNumbers")
 
         println("Введите код из SMS")
-        var userAnswer: String = readln()
+        val userAnswer = readln().toInt()
         if (userAnswer == smsNumbers) {
             print("Поздравляю! Код введен верно!")
         } else {
-            smsNumbers = (1..9).random().toString()
-            println("Код введен неверно, попробуйте снова.")
-            println()
+            smsNumbers = (1000..9999).random()
+            println("Код введен неверно, попробуйте снова.\n")
         }
     } while (smsNumbers != userAnswer)
 }
