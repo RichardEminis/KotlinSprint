de2072058134edea15e3e649d31111287b2b18aa
Alexandr Belyaev <eminis.ru@gmail.com>
2023-08-07 21:21:38 +0300

    KS-8-4 Ingredients list changer (Edited)

--- src/main/kotlin/lesson8/lesson8_4.kt
+++ src/main/kotlin/lesson8/lesson8_4.kt
@@ -1,6 +1,6 @@
 package lesson8
 
-fun main () {
+fun main() {
     var arrayOfIngredients = arrayOf("Морковь", "Свекла", "Помидоры", "Мазик", "Соль", "Любовь")
 
     for (i in arrayOfIngredients) {
@@ -10,18 +10,21 @@ fun main () {
     println("Введите ингредиент, который хотели бы заменить:")
     val ingredient = readln()
 
-    if (arrayOfIngredients.contains(ingredient) != true){
-        println("Такого ингредиента в рецепте нет")
-        return
-    }
-
-    println("Введите название ингредиента, который хотели бы добавить")
-    val newIngredient = readln()
-
-    arrayOfIngredients[arrayOfIngredients.indexOf(ingredient)] = newIngredient
-
-    println("Готово! Вы сохранили следующий список:")
     for (i in arrayOfIngredients) {
-        println(i)
+        if (ingredient.equals(i, ignoreCase = true)) {
+            println("Введите название ингредиента, который хотели бы добавить")
+            val newIngredient = readln()
+
+            arrayOfIngredients[arrayOfIngredients.indexOf(i)] = newIngredient
+
+            println("Готово! Вы сохранили следующий список:")
+            for (i in arrayOfIngredients) {
+                println(i)
+            }
+            return
+        }
     }
+    println("Такого ингредиента в рецепте нет")
 }
+
+
