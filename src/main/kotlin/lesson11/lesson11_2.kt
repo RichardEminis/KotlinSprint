2b01a57929292fe5dfbf0d50d78b38236d39ad57
Alexandr Belyaev <eminis.ru@gmail.com>
2023-08-14 23:13:30 +0300

    KS-11-2 Upgrade UserData Class (Edited)

--- src/main/kotlin/lesson11/lesson11_2.kt
+++ src/main/kotlin/lesson11/lesson11_2.kt
@@ -5,12 +5,48 @@ fun main() {
         logIn = "Ivan",
         password = "qwert",
         email = "Ivan@gmail.com",
-        bio = ""
     )
 
     userData1.addBio()
     userData1.editPassword()
     userData1.printUserData()
     userData1.sendMessage()
+}
+
+class UserData(
+    val logIn: String,
+    var password: String,
+    val email: String,
+    var bio: String? = null
+) {
+
+    fun printUserData() {
+        println(logIn)
+        println(password)
+        println(email)
+        println(bio)
+    }
+
+    fun addBio() {
+        println("Введите информацию о себе:")
+        bio = readln()
+    }
+
+    fun editPassword() {
+        println("Для изменения пароля, введите старый пароль:")
+        if (password == readln()) {
+            println("Введите новый пароль:")
+            password = readln()
+
+            println("Пароль изменен успешно.\n")
+        } else {
+            println("Пароль введен неверно.\n")
+        }
+    }
+
+    fun sendMessage() {
+        println("Введите сообщение для отправки на почту $email:")
+        println("\n$logIn, ${readln()}")
+    }
 }
 
