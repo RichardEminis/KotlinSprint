b1758cdf8e8733035503794a37eb4dc561275059
Alexandr Belyaev <eminis.ru@gmail.com>
2023-08-16 21:45:19 +0300

    KS-11-3 MeetingRooms (Edited)

--- src/main/kotlin/lesson11/lesson11_3.kt
+++ src/main/kotlin/lesson11/lesson11_3.kt
@@ -1,15 +1,36 @@
 package lesson11
 
 fun main() {
-    val meetingRoom1 = MeetingRooms(
+    val meetingRoom1 = MeetingRoom(
         name = "OFFICE",
         cover = "Картинка с работягами",
-        countOfUsers = 6
+        listOfMembers = listOf(Member(name = "Борис", avatar = "Лицо работяги"),
+            Member(name = "Коля", avatar = "Лицо грустного работяги"))
     )
 
-    val meetingRoom2 = MeetingRooms(
+    val meetingRoom2 = MeetingRoom(
         name = "SCHOOL ROOM",
         cover = "Картинка с животными",
-        countOfUsers = 30
+        listOfMembers = listOf(Member(name = "Вася", avatar = "Лицо школьника"),
+            Member(name = "Рома", avatar = "Просто лицо"))
     )
 }
+
+class MeetingRoom(
+    val name: String,
+    var cover: String,
+    val listOfMembers: List<Member>
+) {
+    fun nicknameHighlight() {
+        println("Подсвечивает аватар")
+    }
+
+    fun printStatus() {
+        val listOfStatus = listOf("разговаривает", "микрофон выключен", "пользователь заглушен")
+    }
+}
+
+class Member(
+    val name: String,
+    val avatar: String
+)
