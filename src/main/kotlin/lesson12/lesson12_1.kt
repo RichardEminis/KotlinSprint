26bee066c2f5ab6c4bb4b0173141347e698fb5c2
Alexandr Belyaev <eminis.ru@gmail.com>
2023-08-17 09:23:42 +0300

    KS-12-1 Create Weather Class (Edited)

--- src/main/kotlin/lesson12/lesson12_1.kt
+++ src/main/kotlin/lesson12/lesson12_1.kt
@@ -5,10 +5,19 @@ fun main() {
     weather.isNight = false
     weather.isRainy = false
     weather.atmospherePressure = 30
+
+    weather.printDataOfWeather()
 }
 
 class Weather {
     var isNight = true
     var isRainy = true
     var atmospherePressure = 20
+
+    fun printDataOfWeather() {
+        println("Сейчас ночное время суток: $isNight")
+        println("Сейчас идет дождь: $isRainy")
+        println("Атмосферное давление: $atmospherePressure")
+    }
 }
+
