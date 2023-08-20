235d3d2b926ce66ee6ddb5e39bc32b5390608a77
Alexandr Belyaev <eminis.ru@gmail.com>
2023-08-20 17:32:28 +0300

    KS-12-2 (Edit)

--- src/main/kotlin/lesson12/lesson12_2.kt
+++ src/main/kotlin/lesson12/lesson12_2.kt
@@ -1,23 +1,26 @@
 package lesson12
 
 fun main() {
-    val weather = StatisticOfWeather()
-    weather.isNight = false
-    weather.isRainy = false
-    weather.atmospherePressure = 30
+    val weatherOnMonday = StatisticOfWeather(true, false, 34)
+    weatherOnMonday.printDataOfWeather()
 
-    weather.printDataOfWeather()
+    val weatherOnTuesday = StatisticOfWeather(false, false, 40)
+    weatherOnTuesday.printDataOfWeather()
+
+    val weatherOnWednesday = StatisticOfWeather(true, true, 51)
+    weatherOnWednesday.printDataOfWeather()
+
 }
 
-class StatisticOfWeather {
-    var isNight = true
-    var isRainy = true
-    var atmospherePressure = 20
+class StatisticOfWeather(isNight: Boolean, isRainy: Boolean, atmospherePressure: Int) {
+    var isNight = isNight
+    var isRainy = isRainy
+    var atmospherePressure = atmospherePressure
 
     fun printDataOfWeather() {
         println("Сейчас ночное время суток: $isNight")
         println("Сейчас идет дождь: $isRainy")
-        println("Атмосферное давление: $atmospherePressure")
+        println("Атмосферное давление: $atmospherePressure\n")
     }
 }
 
