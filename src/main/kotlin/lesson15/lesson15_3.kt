5f669e90aeab13393804d7c0b57c517b5647f91f
Eminis <eminis.ru@gmail.com>
2023-10-03 00:06:27 +0500

    KS-15-3 Create Class of Administrator and Users. Create method of delete Users And messages.

--- src/main/kotlin/lesson15/lesson15_3.kt
+++ src/main/kotlin/lesson15/lesson15_3.kt
@@ -2,16 +2,44 @@ package lesson15
 
 fun main() {
 
+    val user1 = User(
+        name = "Bob",
+        listOfMessages = mutableListOf("Привет! Как дела?", "Сколько тебе лет?")
+    )
+
+    val user2 = User(
+        name = "Bill"
+    )
+
+    val user3 = User(
+        name = "Johan"
+    )
+
+    val admin = Administrator(
+        listOfUsers = mutableListOf(user1, user2, user3),
+        listOfMessages = mutableListOf()
+    )
+
+    user1.readMessage()
+    admin.deleteMessage(user1.listOfMessages)
+    user1.readMessage()
+    admin.printListOfUsers()
+    admin.deleteUser()
+    admin.printListOfUsers()
 }
 
+
 abstract class Account(
-    var name: String
-){
-    fun readMessage(message: String){
-        println("Вам пришло сообщение:\n$message")
+    var name: String,
+    var listOfMessages: MutableList<String>
+) {
+    fun readMessage() {
+        for (message in listOfMessages) {
+            println(message)
+        }
     }
 
-    fun sendMessage(): String{
+    fun sendMessage(): String {
         println("Введите сообщение для отправки:")
         val messageToSend = readln()
         println("Сообщение отправлено!")
@@ -19,21 +47,37 @@ abstract class Account(
     }
 }
 
-class User (
-    name: String
-) : Account (name)
+class User(
+    name: String,
+    listOfMessages: MutableList<String> = mutableListOf()
+) : Account(name, listOfMessages)
 
 class Administrator(
-    name: String,
-    var listOfUsers: List<User> = listOfNotNull()
-): Account (name) {
-    fun deleteUser(){
+    name: String = "ADMINISTRATOR",
+    var listOfUsers: MutableList<User>,
+    listOfMessages: MutableList<String>
+
+) : Account(name, listOfMessages) {
+    fun deleteUser() {
         println("Введите имя пользователя, которого хотите удалить:")
-        val deleteUser = readln()
-        for (user in listOfUsers)
+        var deleteUser = readln()
+        var indexOfDelete = 0
+        for (user in listOfUsers) {
+            if (user.name == deleteUser)
+                indexOfDelete = listOfUsers.indexOf(user)
+        }
+        listOfUsers.removeAt(indexOfDelete)
+    }
 
-    fun deleteMessage(){
+    fun deleteMessage(messages: MutableList<String>) {
+        println("Введите индекс сообшения, которое хотите удалить:")
+        val indexToRemove = readln().toInt()
+        messages.removeAt(indexToRemove)
+    }
 
+    fun printListOfUsers() {
+        for (i in listOfUsers)
+            println(i.name)
     }
 }
 
