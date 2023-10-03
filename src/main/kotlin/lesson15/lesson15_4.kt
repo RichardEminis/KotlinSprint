1b215aa53ec1ee6becb6d091588fa640e658184e
Eminis <eminis.ru@gmail.com>
2023-10-03 13:07:26 +0500

    KS-15-4 Create Class of Instruments and Accessories

---
+++ src/main/kotlin/lesson15/lesson15_4.kt
@@ -0,0 +1,47 @@
+package lesson15
+
+fun main() {
+
+    val instrument = Instrument(
+        name = "Гитара",
+        amount = 4,
+        listOfAccessories = listOf(
+            Accessories("Ремень", 2), Accessories("Медиатор", 43),
+            Accessories("Тюнер", 10)
+        )
+    )
+
+    instrument.findAccessories()
+
+}
+
+abstract class Product(
+    val name: String,
+    val amount: Int
+) : find
+
+class Instrument(
+    name: String,
+    amount: Int,
+    val listOfAccessories: List<Accessories>
+
+) : Product(name, amount) {
+    override fun findAccessories() {
+        println("Список аксессуаров для $name: ")
+        super.findAccessories()
+        for (i in listOfAccessories) {
+            println("Товар: ${i.name}, Количество: ${i.amount}")
+        }
+    }
+}
+
+class Accessories(
+    name: String,
+    amount: Int
+) : Product(name, amount)
+
+interface find {
+    fun findAccessories() {
+        println("Выполняется поиск...")
+    }
+}
