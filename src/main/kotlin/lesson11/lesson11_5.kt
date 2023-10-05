bfdc82f8a2d609b5f1c8bb5b0a8ff9472db3929c
Eminis <eminis.ru@gmail.com>
2023-10-05 13:01:42 +0200

    KS-11-5 Create Forum Class (Edited)

--- src/main/kotlin/lesson11/lesson11_5.kt
+++ src/main/kotlin/lesson11/lesson11_5.kt
@@ -4,9 +4,28 @@ fun main() {
     val forums = Forum(
         mapOfUser = mutableMapOf()
     )
-    forums.createNewUser(0, "Ban", "asdfg", "asdfg@gmail.com")
-    forums.createNewUser(1, "Sam", "qwert", "qwert@gmail.com")
-    forums.createNewUser(2, "Bob", "zxcvb", "zxcvb@gmail.com")
+
+    val user1 = User(
+        logIn = "Ban",
+        password = "asdfg",
+        email = "asdfg@gmail.com"
+    )
+
+    val user2 = User(
+        logIn = "Sam",
+        password = "qwert@gmail.com",
+        email = "asdfg@gmail.com"
+    )
+
+    val user3 = User(
+        logIn = "Bob",
+        password = "zxcvb",
+        email = "zxcvb@gmail.com"
+    )
+
+    forums.createNewUser(0, user1)
+    forums.createNewUser(1, user2)
+    forums.createNewUser(2, user3)
     forums.createNewMessage(0, "Привет")
     forums.createNewMessage(1, "Здарова")
     forums.createNewMessage(2, "Хочу кушатъ")
@@ -27,8 +46,8 @@ class Forum(
     var mapOfUser: MutableMap<Int, User>,
     var log: String = ""
 ) {
-    fun createNewUser(id: Int, logIn: String, password: String, email: String) {
-        mapOfUser.put(id, User(logIn, password, email))
+    fun createNewUser(id: Int, user: User) {
+        mapOfUser.put(id, user)
     }
 
     fun createNewMessage(authorId: Int, message: String) {
