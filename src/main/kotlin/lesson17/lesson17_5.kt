a8def7b576d025e34daa2bd50ae875c2d09cfd76
Alexandr Belyaev <eminis.ru@gmail.com>
2023-10-08 23:36:57 +0300

    KS-17-5 Create Setter And Getter for LogIn and Password (Edited)

--- src/main/kotlin/lesson17/lesson17_5.kt
+++ src/main/kotlin/lesson17/lesson17_5.kt
@@ -2,31 +2,36 @@ package lesson17
 
 fun main() {
     val user = DefaultUser(
-        name = "Корней Иванович"
+        name = "Корней Иванович",
+        userLogIn = "Gigamaster56",
+        userPassword = "Asdfg123"
     )
     user.password = "sdfs"
-    println("Ваш пароль: ${user.password}")
-    println("Ваш логин: ${user.logIn}")
+    user.password
+    println("Ваш логин: ${user.userLogIn}")
     user.logIn = "SuperGigaMaster56"
-    println("Ваш логин: ${user.logIn}")
+    println("Ваш логин: ${user.userLogIn}")
 }
 
 class DefaultUser(
-    val name: String
+    val name: String,
+    var userLogIn: String,
+    var userPassword: String
 ) {
-    var logIn: String = "Gigamaster56"
+    var logIn: String = userLogIn
         set(value: String) {
             println("Логин успешно изменен!")
-            field = value
+            userLogIn = value
         }
 
-    var password: String = "Asdfg123"
+    var password: String = userPassword
         get() {
-            var stars = ""
+            print("Ваш пароль: ")
             for (i in 0 until field.count()) {
-                stars += "*"
+                print("*")
             }
-            field = stars
+            println()
+            field = ""
             return field
         }
         set(value: String) {
