eaaacb322523de6f657f1e3a36e48d1b4eed3ef0
Eminis <eminis.ru@gmail.com>
2023-10-08 22:52:54 +0200

    KS-18-5 Create Basic Class

---
+++ src/main/kotlin/lesson18/lesson18_5.kt
@@ -0,0 +1,15 @@
+package lesson18
+
+fun main() {}
+
+abstract class Object()
+
+class Square() : Object() {}
+
+class Circle() : Object() {}
+
+class Point() : Object() {}
+
+class Screan() {
+    fun draw() {}
+}
