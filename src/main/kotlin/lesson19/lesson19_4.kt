0c6481201c4d31d4d81e8926aef96c007e90bb93
Belyaev.AA4 <eminis.ru@gmail.com>
2023-10-08 15:06:48 +0300

    KS-19-4 Game: Shooting From a Tank (Edited)

--- src/main/kotlin/lesson19/lesson19_4.kt
+++ src/main/kotlin/lesson19/lesson19_4.kt
@@ -3,8 +3,7 @@ package lesson19
 fun main() {
     val ironTank = Tank(
         name = "Железяка",
-        typeOfAmmo = Ammo.NOTHING,
-        dmg = Ammo.NOTHING.dmg
+        typeOfAmmo = Ammo.NOTHING
     )
     ironTank.equipDemo(Ammo.GREEN)
     ironTank.fire()
@@ -25,19 +24,17 @@ enum class Ammo(val color: String, val dmg: Int) {
 
 class Tank(
     val name: String,
-    var typeOfAmmo: Ammo,
-    var dmg: Int
+    var typeOfAmmo: Ammo
 ) {
     fun equipDemo(ammunition: Ammo) {
         this.typeOfAmmo = ammunition
-        this.dmg = ammunition.dmg
         println("Вооружены следующие патроны: ${ammunition.color}")
     }
 
     fun fire() {
         println("ПРОИЗВЕДЕН ВЫСТРЕЛ!")
         println("____________|]------iiiiiiiD -----iiiiiiiD -----iiiiiiiD") //Это типа пули летят из пушки:D
-        println("Нанесен урон: ${dmg}")
+        println("Нанесен урон: ${typeOfAmmo.dmg}")
         println()
     }
 }
