b9770ec60dcbbef339090b9abf645f239a5d5b10
Alexandr Belyaev <eminis.ru@gmail.com>
2023-10-09 00:07:41 +0300

    KS-18-5 Overload class Screen

--- src/main/kotlin/lesson18/lesson18_5.kt
+++ src/main/kotlin/lesson18/lesson18_5.kt
@@ -10,6 +10,9 @@ class Circle() : Object() {}
 
 class Point() : Object() {}
 
-class Screan() {
-    fun draw() {}
+class Screen() {
+    fun draw(x: Int, y: Int, obj: Object) {}
+    fun draw(x: Int, y: Float, obj: Object) {}
+    fun draw(x: Float, y: Int, obj: Object) {}
+    fun draw(x: Float, y: Float, obj: Object) {}
 }
