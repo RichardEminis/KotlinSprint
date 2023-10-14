d65e423a26b357488f15eeb33582c7d7dbd0eb68
Belyaev.AA4 <eminis.ru@gmail.com>
2023-10-14 18:57:16 +0300

    KS-18-5 Edit class

--- src/main/kotlin/lesson18/lesson18_5.kt
+++ src/main/kotlin/lesson18/lesson18_5.kt
@@ -1,18 +1,56 @@
 package lesson18
 
-fun main() {}
+fun main() {
+    val screen = Screen (
+        listOfObjects = mutableListOf()
+    )
 
-abstract class Object()
+    screen.draw(43,432.43F, Circle())
+}
 
-class Square() : Object() {}
+abstract class Object(
+    val name: String
+    var x: Float,
+    var y: Float
+)
 
-class Circle() : Object() {}
+class Square(
+    name: String = "квадрат",
+    x: Float,
+    y: Float
+) : Object(name, x, y) {}
 
-class Point() : Object() {}
+class Circle(
+    name: String = "круг",
+    x: Float,
+    y: Float
+) : Object(name, x, y) {}
 
-class Screen() {
-    fun draw(x: Int, y: Int, obj: Object) {}
-    fun draw(x: Int, y: Float, obj: Object) {}
-    fun draw(x: Float, y: Int, obj: Object) {}
-    fun draw(x: Float, y: Float, obj: Object) {}
+class Point(
+    name: String = "точка",
+    x: Float,
+    y: Float
+) : Object(name, x, y) {}
+
+class Screen(
+    var listOfObjects: MutableList<Object>
+) {
+    fun draw(x: Int, y: Int, obj: Object) {
+        println("В координатах: X = $x Y = $y нарисован объект: = ${obj.name}")
+        obj.x = x.toFloat()
+        obj.y = y.toFloat()
+        listOfObjects.add(obj)
+    }
+
+    fun draw(x: Int, y: Float, obj: Object) {
+        println("В координатах: X = $x Y = $y нарисован объект: = ${obj.name}")
+    }
+
+    fun draw(x: Float, y: Int, obj: Object) {
+        println("В координатах: X = $x Y = $y нарисован объект: = ${obj.name}")
+    }
+
+    fun draw(x: Float, y: Float, obj: Object) {
+        println("В координатах: X = $x Y = $y нарисован объект: = ${obj.name}")
+    }
 }
