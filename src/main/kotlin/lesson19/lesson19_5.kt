94a186cb8308d956cb904773d324c5a5c444616f
Alexandr Belyaev <eminis.ru@gmail.com>
2023-10-17 09:35:58 +0300

    KS-19-5 Create Basic Class

---
+++ src/main/kotlin/lesson19/lesson19_5.kt
@@ -0,0 +1,21 @@
+package lesson19
+
+fun main() {}
+
+enum class GenderOfPerson(gender: String) {
+    MAN("мужчина"),
+    WOMAN("женщина")
+}
+
+class CardIndex (
+    listOfPersons: Person
+) {
+    fun addPersonToCard(){}
+}
+
+class Person(
+    val name: String,
+    val gender: GenderOfPerson
+)
+
+
