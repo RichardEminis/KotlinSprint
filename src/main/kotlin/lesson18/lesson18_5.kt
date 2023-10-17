c98c75200c632279f401a33922c57fdab4b472a1
Belyaev.AA4 <eminis.ru@gmail.com>
2023-10-17 16:59:37 +0300

    KS-18-5 Overload class Screen

--- src/main/kotlin/lesson18/lesson18_5.kt
+++ src/main/kotlin/lesson18/lesson18_5.kt
@@ -5,47 +5,32 @@ fun main() {
     val square = Square()
     val point = Point()
 
-    val screen = Screen (
-        listOfObjects = mutableListOf()
-    )
+    val screen = Screen()
 
-    screen.draw(43,432.43F, circle1)
+    screen.draw(43, 432.43F, circle1)
     screen.draw(43.34F, 234, square)
     screen.draw(24.43F, 34.43F, point)
 }
 
 abstract class Object(
-    val name: String,
-    var x: Float,
-    var y: Float
+    val name: String
 )
 
 class Square(
-    name: String = "квадрат",
-    x: Float = 0F,
-    y: Float = 0F
-) : Object(name, x, y) {}
+    name: String = "квадрат"
+) : Object(name)
 
 class Circle(
-    name: String = "круг",
-    x: Float = 0F,
-    y: Float = 0F
-) : Object(name, x, y) {}
+    name: String = "круг"
+) : Object(name)
 
 class Point(
-    name: String = "точка",
-    x: Float = 0F,
-    y: Float = 0F
-) : Object(name, x, y) {}
+    name: String = "точка"
+) : Object(name)
 
-class Screen(
-    var listOfObjects: MutableList<Object>
-) {
+class Screen {
     fun draw(x: Int, y: Int, obj: Object) {
         println("В координатах: X = $x Y = $y нарисован объект: = ${obj.name}")
-        obj.x = x.toFloat()
-        obj.y = y.toFloat()
-        listOfObjects.add(obj)
     }
 
     fun draw(x: Int, y: Float, obj: Object) {
