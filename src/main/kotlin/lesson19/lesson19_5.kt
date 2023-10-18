c0aa26424c1c2f9b4a9cf83aeaf75d58502a7940
Belyaev.AA4 <eminis.ru@gmail.com>
2023-10-18 18:43:02 +0300

    KS-19-5 Create Class CardIndex with adding persons

--- src/main/kotlin/lesson19/lesson19_5.kt
+++ src/main/kotlin/lesson19/lesson19_5.kt
@@ -1,24 +1,40 @@
 package lesson19
 
-fun main() {}
+fun main() {
+    val cardIndex = CardIndex(
+        listOfPersons = mutableListOf()
+    )
 
-enum class GenderOfPerson(gender: String) {
-    MAN("мужчина"),
-    WOMAN("женщина")
+    cardIndex.registration()
+}
+
+enum class GenderOfPerson(val personGender: String) {
+    MAN("Мужчина"),
+    WOMAN("Женщина")
 }
 
 class CardIndex(
-    listOfPersons: Person
+    var listOfPersons: MutableList<Person>
 ) {
-    fun addPersonToCard(name: String, gender: GenderOfPerson) {}
+    fun addPersonToCard(name: String, personGender: GenderOfPerson) {
+        listOfPersons.add(Person(name, personGender))
+    }
+
     fun registration() {
-        println("Введите ваше имя:")
-        val nameAnswer = readln()
-        println("Введите ваш пол\nЕсли вы женщина, введите 'Ж'\nЕсли вы мужчина, введите 'М'")
-        val genderAnswer: GenderOfPerson
-        if (readln().equals("М", ignoreCase = true))
-            genderAnswer = GenderOfPerson.MAN
-        else genderAnswer = GenderOfPerson.WOMAN
+        println("Необходимо ввести в картотеку 5 человек, после чего операция будет завершена.")
+        for (i in 1..5) {
+            println("Введите имя:")
+            val nameAnswer = readln()
+            println("Введите гендер\nЕсли вы женщина, введите 'Ж'\nЕсли вы мужчина, введите 'М'")
+            val genderAnswer: GenderOfPerson
+            if (readln().equals("М", ignoreCase = true))
+                genderAnswer = GenderOfPerson.MAN
+            else genderAnswer = GenderOfPerson.WOMAN
+            addPersonToCard(nameAnswer, genderAnswer)
+        }
+        for (i in listOfPersons) {
+            println("Имя: ${i.name}\nГендер: ${i.gender.personGender}")
+        }
     }
 }
 
