f2aa56544c37a70c7bb9947e7234decb16aad2fa
Belyaev.AA4 <eminis.ru@gmail.com>
2023-11-21 11:32:08 +0300

    KS-11-5 Create Forum Class (Edited)

--- src/main/kotlin/lesson11/lesson11_5.kt
+++ src/main/kotlin/lesson11/lesson11_5.kt
@@ -2,35 +2,16 @@ package lesson11
 
 fun main() {
     val forums = Forum(
-        mapOfUser = mutableMapOf()
+        listOfUser = mutableListOf()
     )
-
-    val user1 = User(
-        logIn = "Ban",
-        password = "asdfg",
-        email = "asdfg@gmail.com"
-    )
-
-    val user2 = User(
-        logIn = "Sam",
-        password = "qwert@gmail.com",
-        email = "asdfg@gmail.com"
-    )
-
-    val user3 = User(
-        logIn = "Bob",
-        password = "zxcvb",
-        email = "zxcvb@gmail.com"
-    )
-
-    forums.createNewUser(0, user1)
-    forums.createNewUser(1, user2)
-    forums.createNewUser(2, user3)
-    forums.createNewMessage(0, "Привет")
-    forums.createNewMessage(1, "Здарова")
-    forums.createNewMessage(2, "Хочу кушатъ")
-    forums.createNewMessage(0, "Я тоже")
-    forums.createNewMessage(1, "И я")
+    forums.createNewUser("Ban", "asdfg", "asdfg@gmail.com")
+    forums.createNewUser("Sam", "qwert", "qwert@gmail.com")
+    forums.createNewUser("Bob", "zxcvb", "zxcvb@gmail.com")
+    forums.createNewMessage(forums.listOfUser[0], "Привет")
+    forums.createNewMessage(forums.listOfUser[1], "Здарова")
+    forums.createNewMessage(forums.listOfUser[2], "Хочу кушатъ")
+    forums.createNewMessage(forums.listOfUser[1], "Я тоже")
+    forums.createNewMessage(forums.listOfUser[0], "И я")
 
     forums.printThread()
 }
@@ -38,20 +19,24 @@ fun main() {
 class User(
     val logIn: String,
     val password: String,
-    val email: String
+    val email: String,
+    var userId: Int? = null
 )
 
 class Forum(
     var id: Int = 0,
-    var mapOfUser: MutableMap<Int, User>,
+    var listOfUser: MutableList<User>,
     var log: String = ""
 ) {
-    fun createNewUser(id: Int, user: User) {
-        mapOfUser.put(id, user)
+    fun createNewUser(logIn: String, password: String, email: String): User {
+        val user = User(logIn, password, email, userId = id)
+        listOfUser.add(user)
+        id++
+        return user
     }
 
-    fun createNewMessage(authorId: Int, message: String) {
-        log += "${mapOfUser.getValue(authorId).logIn}: ${message}\n"
+    fun createNewMessage(user: User, message: String) {
+        log += "${user.logIn}: ${message}\n"
     }
 
     fun printThread() {
