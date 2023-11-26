17f77b9381e19c5dcdf13f2f9e164cc8bc997ab6
Alexandr Belyaev <eminis.ru@gmail.com>
2023-11-26 22:56:34 +0300

    KS-21-1 Create extension function for Class String (Edited)

--- src/main/kotlin/lesson21/lesson21_1.kt
+++ src/main/kotlin/lesson21/lesson21_1.kt
@@ -1,16 +1,10 @@
 package lesson21
 
-import java.util.*
-
 fun main() {
     val word = "Hello World!"
     word.vowelCounter()
 }
 
 fun String.vowelCounter() {
-    var count = 0
-    val vocals = "aeiouy"
-
-    this.lowercase(Locale.getDefault()).groupingBy { if (vocals.contains(it)) count++ }.eachCount()
-    println("Количество гласных: $count")
+    println("Количество гласных: ${this.lowercase().count({ letter -> "aeiouy".contains(letter) })}")
 }
