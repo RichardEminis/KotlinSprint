dfeb4515c9928d56a462e75eda2a3ba753471789
Alexandr Belyaev <eminis.ru@gmail.com>
2023-12-08 16:07:37 +0300

    KS-22-1 (Edited)

--- src/main/kotlin/lesson22/lesson22_1.kt
+++ src/main/kotlin/lesson22/lesson22_1.kt
@@ -1,9 +1,12 @@
 package lesson22
 
 fun main() {
-    val first = DataBook("Владимир С.А.", "Буревестник")
-    val second = RegularBook("Владимир С.А.", "Буревестник")
-    println(first == second) //невозможно сравнить два разных по типу экземпляра класса, сравнение возможно только если эземпляры класса принадлежат одному классу
+    val dataBook0 = DataBook("Владимир С.А.", "Буревестник")
+    val dataBook1 = DataBook("Владимир С.А.", "Буревестник")
+    val regularBook0 = RegularBook("Владимир С.А.", "Буревестник")
+    val regularBook1 = RegularBook("Владимир С.А.", "Буревестник")
+    println(dataBook0 == dataBook1) // значение true, так как hashCode у объектов одинаковый и сравнение происходит внутри класса
+    println(regularBook0 == regularBook1) // значение false, так как hashCode у объектов разный и не сравниваются непосредственно сами поля
 }
 
 data class DataBook(
