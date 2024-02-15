5b962b755451488b741e4a0349f4d2e107b9ee29
Alexandr Belyaev <eminis.ru@gmail.com>
2024-02-15 18:26:58 +0300

    KS-22-5 Edit with using component function

--- src/main/kotlin/lesson22/lesson22_5.kt
+++ src/main/kotlin/lesson22/lesson22_5.kt
@@ -1,14 +1,8 @@
 package lesson22
 
 fun main() {
-    val alphaCentauri = GalacticGuide(
-        name = "Alpha Centauri",
-        description = "Ближайшая к Солнечной системе звездная система, состоящая из трех звезд: Альфа Центавра А, Альфа Центавра В и Проксима Центавра.",
-        dateTime = "1978.06.17",
-        distanceFromEarth = 4.37
-    )
 
-    val (name, description, dateTime, distanceFromEarth) = alphaCentauri
+    val (name, description, dateTime, distanceFromEarth) = getGalacticGuide()
 
     printData(name, description, dateTime, distanceFromEarth)
 }
@@ -19,6 +13,15 @@ data class GalacticGuide(
     val dateTime: String,
     val distanceFromEarth: Double
 )
+
+fun getGalacticGuide(): GalacticGuide {
+    return GalacticGuide(
+        "Alpha Centauri",
+        "Ближайшая к Солнечной системе звездная система, состоящая из трех звезд: Альфа Центавра А, Альфа Центавра В и Проксима Центавра.",
+        "1978.06.17",
+        4.37
+    )
+}
 
 fun printData(name: String, description: String, dateTime: String, distanceFromEarth: Double) {
     println("Название: $name")
